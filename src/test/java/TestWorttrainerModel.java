af8eb2697378f29dedb026415b3c1852dcca5454
bgogoladze <bgogoladze@student.tgm.ac.at>
2024-10-02 16:27:04 +0200

    [GK] Worttrainer Reloaded Commit 10: Nun geschafft, das Problem zu lösen mit gradle, project-structure, das Problem lag an den Java Folders welche nicht generiert wurden. Nun wird das Testen fortgesetzt.

--- src/test/java/TestWorttrainerModel.java
+++ src/test/java/TestWorttrainerModel.java
@@ -28,4 +28,11 @@ public class TestWorttrainerModel {
         assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", ""), "Es wurde versucht eine leere Url hinzuzufügen");
         assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", "hhtp://www.google.com"), "Es wurde versucht invalide Url zum Papagei Bild als Value hinzuzufügen");
     }
+    @Test
+    @DisplayName("U01 - Eine WortListe welche aus invaliden Zeichen besteht wirft eine Exception")
+    public void x() {
+        assertThrows(IllegalArgumentException.class, () -> new WortListe(null, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vetline.de%2Fbitte-abdunkeln-so-schlafen-wellensittiche-besser&psig=AOvVaw2hwxvnhElmDjiWnhOSq5q-&ust=1727960711856000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjsvrjh74gDFQAAAAAdAAAAABAE"), "Es wurde versucht das Wort als null-Wert zu speichern");
+        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", ""), "Es wurde versucht eine leere Url hinzuzufügen");
+        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", "hhtp://www.google.com"), "Es wurde versucht invalide Url zum Papagei Bild als Value hinzuzufügen");
+    }
 }
