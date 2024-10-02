080c96e4f24aeb7bc9de6cca40b02bdccc44952d
bgogoladze <bgogoladze@student.tgm.ac.at>
2024-10-02 16:37:22 +0200

    [GK] Worttrainer Reloaded Commit 11: Nun wurde bereits eine Testmethode für die Konstuktoren erstellt um sicherzustellen, dass keine null-Werte, ungültige oder leere Werte vorhanden sind.

--- src/test/java/TestWorttrainerModel.java
+++ src/test/java/TestWorttrainerModel.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -26,13 +27,13 @@ public class TestWorttrainerModel {
     public void notNullEmptyInvalid() {
         assertThrows(IllegalArgumentException.class, () -> new WortListe(null, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vetline.de%2Fbitte-abdunkeln-so-schlafen-wellensittiche-besser&psig=AOvVaw2hwxvnhElmDjiWnhOSq5q-&ust=1727960711856000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjsvrjh74gDFQAAAAAdAAAAABAE"), "Es wurde versucht das Wort als null-Wert zu speichern");
         assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", ""), "Es wurde versucht eine leere Url hinzuzufügen");
-        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", "hhtp://www.google.com"), "Es wurde versucht invalide Url zum Papagei Bild als Value hinzuzufügen");
+        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", "hhtp://www.google-com"), "Es wurde versucht invalide Url zum Papagei Bild als Value hinzuzufügen");
     }
+
     @Test
-    @DisplayName("U01 - Eine WortListe welche aus invaliden Zeichen besteht wirft eine Exception")
-    public void x() {
-        assertThrows(IllegalArgumentException.class, () -> new WortListe(null, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vetline.de%2Fbitte-abdunkeln-so-schlafen-wellensittiche-besser&psig=AOvVaw2hwxvnhElmDjiWnhOSq5q-&ust=1727960711856000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjsvrjh74gDFQAAAAAdAAAAABAE"), "Es wurde versucht das Wort als null-Wert zu speichern");
-        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", ""), "Es wurde versucht eine leere Url hinzuzufügen");
-        assertThrows(IllegalArgumentException.class, () -> new WortListe("Papagei", "hhtp://www.google.com"), "Es wurde versucht invalide Url zum Papagei Bild als Value hinzuzufügen");
+    @DisplayName("U02 - ")
+    public void show() {
+        this.wortListe.addWortEintrag("Papagei", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vetline.de%2Fbitte-abdunkeln-so-schlafen-wellensittiche-besser&psig=AOvVaw2hwxvnhElmDjiWnhOSq5q-&ust=1727960711856000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjsvrjh74gDFQAAAAAdAAAAABAE");
+        assertEquals("[", );
     }
 }
