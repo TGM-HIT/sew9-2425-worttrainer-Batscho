9d465dac8958d41a4a71403e3648ac9ecd98f17f
bgogoladze <bgogoladze@student.tgm.ac.at>
2024-10-11 12:21:08 +0200

    [GK] Worttrainer Reloaded Commit 24: Es wurde bereits der Konstruktor erstellt und auch der Randomizer für den aktuellen Worteintrag begonnen.

--- src/main/java/bgogoladze/Model/WortTrainer.java
+++ src/main/java/bgogoladze/Model/WortTrainer.java
@@ -1,5 +1,7 @@
 package bgogoladze.Model;
 
+import java.util.Random;
+
 /**
  * Diese Klasse namens WortTrainer beinhaltet einen einen Standartkonstruktor, Attribute für die WortListe (Wort-Bild-Pairs),
  * den aktuellen Worteintrag, die Anzahl an abgefragten Fragen und korrekt beanworteten Fragen, sowie ein abgeleitetes Attribut
@@ -17,4 +19,31 @@ public class WortTrainer {
     private int abgefragt;              //Die Anzahl an Wörtern die abgefragt wurden
     private int korrekt;                //Die Anzahl der abgefragten Wörter die erraten wurden
     private StringBuilder statistic;    //Ein abgeleitetes Attribut welches die Statistik angibt
+
+    /**
+     * Dies ist der Konstruktor bei dem die übergebenen Parameter in die Attrribute gespeichert werden
+     * @param wortListe ist die Wortliste die gesetzt wird um die Wort-Bild Paare zu verwalten
+     */
+    public WortTrainer(WortListe wortListe) {
+        if(wortListe != null) {
+            this.wortListe = wortListe;
+        } else {
+            throw new NullPointerException("Die übergebene WortListe darf nicht null sein!");
+        }
+    }
+
+    /**
+     * Diese Methode randomWortEintrag liefert einen zufälligen Worteintrag welcher drüber aufgerufen werden kann
+     * @return den zufällig ausgewählten WortEintrag
+     */
+    public String[] randomWortEintrag() {
+        Random zufall = new Random();
+        String[] all = this.wortListe.getWortListe().keySet().toArray(new String[0]);
+        this.aktuell = wortListe.getWortEintrag(all[zufall.nextInt(all.length)]);
+        return this.aktuell;
+        /*do {
+            this.aktuell = this.wortListe.getWortEintrag((int) (Math.random() * this.wortListe.getWortEinträge().length));
+        } while(this.aktuell == null);
+        return this.aktuell;*/
+    }
 }
